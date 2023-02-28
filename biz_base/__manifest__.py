edb4b5ba47bd0ba8090000de3607f8d6238069ba
TheVy <ntvy2k@gmail.com>
2023-02-28 10:10:25 +0700

    application = True

--- biz_base/__manifest__.py
+++ biz_base/__manifest__.py
@@ -1,34 +1,29 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Biz Card",
-
-    'summary': """
+    "name": "Biz Card",
+    "summary": """
         Biz  card is product of company, with Biz card you can customize contact card and portfolio website""",
-
-    'description': """
+    "description": """
          Biz  card is product of company, with Biz card you can customize contact card and portfolio website
     """,
-
-    'author': "T4Tek Team",
-    'website': "https://t4tek.co",
-
+    "author": "T4Tek Team",
+    "website": "https://t4tek.co",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Productivity/BizCard',
-    'version': '16.0.1.0.0',
-
+    "category": "Productivity/BizCard",
+    "version": "16.0.1.0.0",
     # any module necessary for this one to work correctly
-    'depends': ['base'],
-
+    "depends": ["base"],
     # always loaded
-    'data': [
+    "data": [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        "views/views.xml",
+        "views/templates.xml",
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+    "demo": [
+        "demo/demo.xml",
     ],
+    "application": True,
 }
