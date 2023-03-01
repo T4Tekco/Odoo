cc5dafa65da9046080683b49f2e0d557adc4cc82
TheVy <ntvy2k@gmail.com>
2023-03-01 15:03:35 +0700

    Rename

--- t4contact/__manifest__.py
+++ t4contact/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    "name": "Biz Card",
+    "name": "T4 Contact",
     "summary": """
         Biz  card is product of company, with Biz card you can customize contact card and portfolio website""",
     "description": """
@@ -8,14 +8,14 @@
     """,
     "author": "T4Tek Team",
     "website": "https://t4tek.co",
-    "category": "Productivity/BizCard",
+    "category": "Productivity/T4Contact",
     "version": "16.0.1.0.0",
     "depends": ["base"],
     "data": [
         "security/biz_security.xml",
         "security/ir.model.access.csv",
         "views/res_partner_view.xml",
-        "views/industry_view.xml"
+        "views/industry_view.xml",
     ],
     "demo": [
         "demo/demo.xml",
