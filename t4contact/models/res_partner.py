2ff5a7b1e81cdc73aa4090090670bd95f913c9fc
TheVy <ntvy2k@gmail.com>
2023-03-01 16:01:16 +0700

    add fields: charter_capital,owner_ids,legal_representative_ids,identity

--- t4contact/models/res_partner.py
+++ t4contact/models/res_partner.py
@@ -12,10 +12,34 @@ class Contact(models.Model):
     industry_ids = fields.Many2many(
         "biz.industry", relation="industry_rel", string="Sub Industries"
     )
+
+    # ----------
     phone_ids = fields.One2many("biz.phone", "company_id", string="List Phone")
     fax_ids = fields.One2many("biz.fax", "company_id", string="List Fax")
     website_ids = fields.One2many("biz.website", "company_id", string="Websites")
     email_ids = fields.One2many("biz.email", "company_id", string="List Email")
+
+    # --- Currency
+    currency_id = fields.Many2one("res.currency")
+    charter_capital = fields.Monetary("Charter Capital", "currency_id")
+
+    # --- Hell
+    owner_ids = fields.Many2many(
+        "res.partner",
+        relation="owner_company_rel",
+        column1="company_id",
+        column2="owner_id",
+        string="Owners",
+    )
+    legal_representative_ids = fields.Many2many(
+        "res.partner",
+        relation="legal_company_rel",
+        column1="company_id",
+        column2="legal_rep_id",
+        string="Legal Representatives",
+    )
+
+    identity = fields.Char("Identity ID/Business Code")
 
     # TODO: Add following fields:
     # Vietnam name,
