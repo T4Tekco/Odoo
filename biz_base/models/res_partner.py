cfc905992bfa2e291cc50d90932b20f31a5de4ea
TheVy <ntvy2k@gmail.com>
2023-03-01 08:38:20 +0700

    correct one2many fields, changed widget attr in view

--- biz_base/models/res_partner.py
+++ biz_base/models/res_partner.py
@@ -7,14 +7,12 @@ class Contact(models.Model):
     _description = "Biz Info"
     _inherit = "res.partner"
 
-    main_industry_id = fields.Many2one(
-        "biz.industry", required=True, string="Main Industry"
-    )
+    main_industry_id = fields.Many2one("biz.industry", string="Main Industry")
 
     industry_ids = fields.Many2many(
         "biz.industry", relation="industry_rel", string="Sub Industries"
     )
-    phone_ids = fields.One2many("biz.phone", "phone", string="List Phone")
-    fax_ids = fields.One2many("biz.fax", "fax", string="List Fax")
-    website_ids = fields.One2many("biz.website", "website", string="Websites")
-    email_ids = fields.One2many("biz.email", "email", string="List Email")
+    phone_ids = fields.One2many("biz.phone", "company_id", string="List Phone")
+    fax_ids = fields.One2many("biz.fax", "company_id", string="List Fax")
+    website_ids = fields.One2many("biz.website", "company_id", string="Websites")
+    email_ids = fields.One2many("biz.email", "company_id", string="List Email")
