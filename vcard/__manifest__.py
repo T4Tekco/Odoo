07138bf0245000f584bd1ed6ec98bd070ae2bffa
TheVy <ntvy2k@gmail.com>
2023-03-02 16:27:09 +0700

    Prepare

--- vcard/__manifest__.py
+++ vcard/__manifest__.py
@@ -1,35 +1,23 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "V Card",
-
-    'summary': """
+    "name": "V Card",
+    "summary": """
        Render the data in database to vcard format""",
-
-    'description': """
+    "description": """
         This module is used to render the data in database to vcard format
     """,
-
-    'author': "T4Tek",
-    'website': "https://t4tek.co/",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Productivity/Vcard',
-    'version': '16.0.1.0.0',
-
-    # any module necessary for this one to work correctly
-    'depends': ['t4contact'],
-
-    # always loaded
-    'data': [
+    "author": "T4Tek Team",
+    "website": "https://t4tek.co/",
+    "category": "Productivity/Vcard",
+    "version": "16.0.1.0.0",
+    "depends": ["t4contact"],
+    "data": [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        "views/views.xml",
+        "views/templates.xml",
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+    "demo": [
+        "demo/demo.xml",
     ],
     "application": False,
 }
---
+++ vcard/models/vcard.py
@@ -0,0 +1,6 @@
+from odoo import fields, models
+
+
+# class VCard(models.Model):
+#     _name = "biz.vcard"
+#     _description = "VCard"
