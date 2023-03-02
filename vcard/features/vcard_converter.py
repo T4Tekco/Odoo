13c84fa39ae0e329f63a8d35247956e3db6768d7
TheVy <ntvy2k@gmail.com>
2023-03-02 23:29:01 +0700

    add FN, N, EMAIL, ORG, TEL

--- vcard/features/vcard_converter.py
+++ vcard/features/vcard_converter.py
@@ -1,9 +1,10 @@
 """
+Ref: https://www.rfc-editor.org/rfc/rfc6350
 Format:
 
 BEGIN:VCARD
 VERSION: 2.1|3.0|4.0
-<Add data>
+<data here>
 END:VCARD
 
 ----
@@ -11,20 +12,20 @@ END:VCARD
 ADR
 BDAY
 CATEGORIES
-EMAIL
-FN
+- EMAIL
+- FN
 GEO
 KEY
 LOGO
-N
+- N:<last_name>;<first_name>;<middle_name>;<honorific_prefixes>;<honorific_suffixes>
 NOTE
-ORG
+- ORG
 PHOTO
 REV
 ROLE
 SOUND
 SOURCE
-TEL
+- TEL
 TITLE
 TZ
 UID
@@ -38,7 +39,6 @@ CALURI
 CLASS
 CLIENTPIDMAP
 FBURL
-FN
 GENDER
 IMPP
 KIND
@@ -56,50 +56,151 @@ XML
 """
 
 
-class VCardBase:
-    pass
+class VCard:
+    """
+    Common (All versions)
 
+    TODO: ADR, BDAY, CATEGORIES, EMAIL, FN, GEO, KEY, LOGO, N, NOTE, ORG, PHOTO
+    REV, ROLE, SOUND, SOURCE, TEL, TITLE, TZ, UID, URL
 
-class VCard21(VCardBase):
-    pass
+    DONE: FN, N, EMAIL, ORG, TEL
+    """
 
+    version = ""
 
-class VCard30(VCardBase):
-    pass
+    def __init__(self, data):
+        self._data = data
 
+    def _N(
+        self,
+        first_name: str,
+        last_name: str = "",
+        middle_name: str = "",
+        honorific_prefixes: str = "",
+        honorific_suffixes: str = "",
+    ):
+        return f"N:{last_name};{first_name};{middle_name};{honorific_prefixes};{honorific_suffixes}\n"
 
-class VCard40(VCardBase):
-    pass
+    def _FN(self, full_name: str):
+        return f"FN:{full_name}\n"
 
+    def _ORG(self, org_name: str = "", *ou: str):
+        return f"ORG:{org_name};" + ";".join(ou) + "\n" if org_name else ""
 
-class VCardConverter:
-    def __init__(self, data) -> None:
-        self.__data = data
+    def _EMAIL(self, *elements):
+        """
+        element:
+        {
+            "email": str
+            "type": str
+        }
+        """
+
+        return (
+            "".join(f"EMAIL;TYPE={e['type']}:{e['email']}\n" for e in elements)
+            if elements
+            else ""
+        )
+
+    def _TEL(self, *elements):
+        """
+        type-param-tel = "text" / "voice" / "fax" / "cell" / "video" / "pager" / "textphone" / iana-token / x-name
+        ;type-param-tel MUST NOT be used with a property other than TEL.
+
+        element:
+        {
+            type: str,
+            phone: str,
+        }
+        """
+
+        return (
+            "".join(f'TEL;TYPE={",".join(e["type"])}:{e["phone"]}\n' for e in elements)
+            if elements
+            else ""
+        )
+
+    def _BDAY(self):
+        pass
+
+    def _CATEGORIES(self):
+        pass
+
+    def _TITLE(self):
+        pass
+
+    def _UID(self):
+        pass
+
+    def _URL(self):
+        pass
+
+    def _build_data(self):
+        if self.version not in ("2.1", "3.0", "4.0"):
+            raise ValueError("Version must be '2.1' or '3.0' or '4.0'")
+
+        output = (
+            self._N(**self._data["N"])
+            + self._FN(self._data["FN"])
+            + self._ORG(*self._data["ORG"])
+            + self._EMAIL(*self._data["EMAIL"])
+            + self._TEL(*self._data["TEL"])
+        )
+
+        return f"VERSION:{self.version}\n" + output
 
     def build_data(self):
-        output = f"""BEGIN:VCARD
-VERSION:{self.__data['version']}
-N:{self.__data['last_name']};{self.__data['first_name']};;{self.__data['role']}
-TEL;cell:{self.__data['phone']}
-TEL;home:{self.__data['phone']}
-TEL;work:{self.__data['phone']}
-TEL;t4tek:{self.__data['phone']}
-END:VCARD"""
+        return f"BEGIN:VCARD\n{self._build_data()}\nEND:VCARD"
 
-        return output
+
+class VCard21(VCard):
+    version = "2.1"
+
+
+class VCard30(VCard):
+    version = "3.0"
+
+
+class VCard40(VCard):
+    version = "4.0"
+
+
+V_TYPE = {
+    "2.1": VCard21,
+    "3.0": VCard30,
+    "4.0": VCard40,
+}
+
+
+class Converter:
+    def __init__(self, data: dict) -> None:
+        if data.get("version") in V_TYPE:
+            self.vcard: VCard = V_TYPE.get(data["version"])(data)  # type:ignore
+        else:
+            raise KeyError("Version ... 2.1 | 3.0 | 4.0")
+
+    def _convert(self):
+        return self.vcard.build_data()
 
     def write_to_vcf(self):
         with open("out.vcf", "w") as f:
-            f.write(self.build_data())
+            f.write(self._convert())
 
 
 if __name__ == "__main__":
     data = {
         "version": "4.0",
-        "last_name": "Nguyen",
-        "first_name": "Vy",
-        "role": "PhD",
-        "phone": "+84987654321",
+        "N": {"first_name": "Vy", "last_name": "Nguyen", "middle_name": "The"},
+        "FN": "Nguyen The Vy",
+        "ORG": ("T4Tek", "Vua Backend", "Hoang De Odoo"),
+        "EMAIL": (
+            {"email": "abc@t4tek.co", "type": "work"},
+            {"email": "abc2@t4tek.co", "type": "home"},
+        ),
+        "TEL": (
+            {"phone": "+84987654321", "type": ("home", "work", "business")},
+            {"phone": "+84987654333", "type": ("cell",)},
+        ),
     }
-    v = VCardConverter(data)
-    print(v.write_to_vcf())
+    v = Converter(data)
+    v.write_to_vcf()
