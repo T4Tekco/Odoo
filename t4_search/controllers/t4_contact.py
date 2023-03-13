bebf86d29aa46a215e79a5a265ea75273732f452
TheVy <ntvy2k@gmail.com>
2023-03-13 22:43:24 +0700

    result -> dropdown

--- t4_search/controllers/t4_contact.py
+++ t4_search/controllers/t4_contact.py
@@ -7,8 +7,9 @@ _logger = logging.getLogger(__name__)
 
 
 class T4Search(http.Controller):
-    @http.route("/s/contacts/", auth="public", type="json", methods=["GET"])
-    def fetch_contact(self):
-        _logger.info(http.request.env.user)
+    @http.route("/s/contacts/", auth="public", type="json", methods=["GET", "POST"])
+    def fetch_contact(self, **kw):
+        _logger.info(kw)
+        _logger.info(kw.get("query"))
         contacts = http.request.env["res.partner"].sudo().search_read([], ("name",))
         return contacts
--- t4_search/static/src/snippets/s_t4_contact_searchbar/000.js
+++ t4_search/static/src/snippets/s_t4_contact_searchbar/000.js
@@ -1,6 +1,7 @@
 /** @odoo-module **/
 
 import public_widget from "web.public.widget";
+import { qweb } from "web.core";
 
 export default public_widget.registry.t4_contact_searchbar =
   public_widget.Widget.extend({
@@ -10,8 +11,55 @@ export default public_widget.registry.t4_contact_searchbar =
      */
     start: function () {
       let _form = this.el;
+      let _input = _form.querySelector(".t4_search_query");
 
-      window.v_form = _form;
+      let _timeout = null;
+
+      _input.oninput = (ev) => {
+        if (_timeout != null) {
+          clearTimeout(_timeout);
+        }
+        _timeout = setTimeout(() => this._search(ev.target.value.trim()), 2000);
+      };
+    },
+
+    _search: function (query) {
+      if (!query) {
+        return;
+      }
+      console.log(query);
+      this._rpc({
+        route: "/s/contacts",
+        params: { query: query },
+      }).then((r) => {
+        this._render(r);
+      });
+    },
+
+    _render: function (data) {
+      let _form = this.el;
+      let template = "t4_search.s_t4_contact_searchbar.autocomplete";
+
+      if (!data.length) {
+        _form.classList.remove("dropdown");
+        _form.classList.remove("show");
+        return;
+      }
+
+      // Rendered
+      console.log(data);
       console.log(_form);
+
+      let output = qweb.render(template, {
+        results: data,
+      });
+      let div = document.createElement("div");
+
+      div.innerHTML = output;
+
+      _form.append(div);
+
+      _form.classList.add("dropdown");
+      _form.classList.add("show");
     },
   });
