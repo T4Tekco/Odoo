09cb271ebf34f0dd93bc5b557f7a6c1e4fe67e02
TheVy <ntvy2k@gmail.com>
2023-03-14 15:17:31 +0700

    init statistic

---
+++ t4_statistic/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "T4 Statistic",
+    "summary": """
+        Contact Statistic""",
+    "description": """
+        Trời tối.
+    """,
+    "author": "T4Tek Team",
+    "website": "https://github.com/T4Tekco/Odoo",
+    "category": "T4/Statistic",
+    "version": "16.0.1.0.0",
+    "depends": ["t4", "t4_contact"],
+    "data": [
+        # 'security/ir.model.access.csv',
+        "views/views.xml",
+        "views/templates.xml",
+    ],
+}
