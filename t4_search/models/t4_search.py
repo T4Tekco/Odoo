0508cffb6fcfd1885e1142868a70a3838ae2218f
Neil_Nguyen <higherblood2309@gmail.com>
2023-03-14 09:19:44 +0700

    delete t4_search

--- t4_search/models/t4_search.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models, api
-
-
-class Search(models.Model):
-    _name = "t4.search"
-    _description = "primacy search"
-
-
-primacy_search = fields.Char(string="primacy")
