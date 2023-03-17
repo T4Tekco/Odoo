65d84a6b6faeddce98fc8ea764511a3fa17f4623
TheVy <ntvy2k@gmail.com>
2023-03-17 09:33:18 +0700

    industry filter

--- t4_search/controllers/search.py
+++ t4_search/controllers/search.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+import functools
 import logging
+from abc import ABC, abstractmethod
 from typing import Iterable
 
 from odoo import http
@@ -7,23 +9,28 @@ from odoo import http
 _logger = logging.getLogger(__name__)
 
 
-class ContactFilter:
-    def __init__(self, query: str) -> None:
-        self.__domain = []
-        self.__domain.append(("name", "ilike", query))
-        self.__domain.append(("privacy_search", "=", True))
+class BaseFilter(ABC):
+    def __init__(self, value: str) -> None:
+        self.domain = []
+        self.initialize(value)
+
+    @abstractmethod
+    def initialize(self, value: str):
+        pass
+
+    def build_domain(self):
+        return self.domain
+
+
+class ContactFilter(BaseFilter):
+    def initialize(self, value: str):
+        self.domain.append(("name", "ilike", value))
+        self.domain.append(("privacy_search", "=", True))
 
     def add_keyword(self, keywords: Iterable[str]):
         for keyword in keywords:
             if k := keyword.strip():
-                self.__domain.append(("category_id.name", "=", k))
-
-    def add_industry(self, industry: str):
-        # TODO: implement
-        pass
-
-    def build_domain(self):
-        return self.__domain
+                self.domain.append(("category_id.name", "=", k))
 
 
 class T4Search(http.Controller):
@@ -41,39 +48,52 @@ class T4Search(http.Controller):
 
             yield contact
 
-    def get_recordset(self, domain, fields, **kw):
-        contacts = (
-            http.request.env["res.partner"]
-            .sudo()
-            .search_read(
-                domain,
-                fields,
-                limit=kw.get("limit", 0),
-            )
-        )
+    def get_recordset(self, domain, **kw):
 
-        return contacts
+        Contact = http.request.env["res.partner"]
+
+        return Contact.sudo().search(domain, limit=kw.get("limit", 0))  # type:ignore
 
     def get_domain(self, **kw):
         query = kw.get("query", "")
         keywords = kw.get("keywords", "").split(",")
-        industry = kw.get("industry", "").strip()
 
         _filter = ContactFilter(query)
         _filter.add_keyword(keywords)
-        _filter.add_industry(industry)
 
         domain = _filter.build_domain()
 
         return domain
 
+    def get_recordset_from_industry(self, _industry: str):
+        if not _industry:
+            return None
+
+        Industry = http.request.env["t4.industry"].search([("code", "=", _industry)])
+
+        contacts = set(Industry.company_ids + Industry.sub_company_ids)
+        if contacts:
+            contacts = functools.reduce(lambda a, b: a + b, contacts)
+
+        return contacts if contacts else None
+
+    def convert_data(self, recordset):
+        fields = self._get_fields()
+        return recordset.sudo().search_read([("id", "in", recordset.ids)], fields)
+
     def _prepare_data(self, **kw):
         _logger.info(f"query: {kw}")
 
-        fields = self._get_fields()
         domain = self.get_domain(**kw)
+        recordset = self.get_recordset(domain)
 
-        contacts = self.get_recordset(domain, fields, **kw)
+        if industry := kw.get("industry", "").strip():
+            if reverse_rs := self.get_recordset_from_industry(industry):
+                recordset = recordset & reverse_rs
+
+        contacts = recordset
+
+        contacts = self.convert_data(contacts)
         contacts = self._process_contact_url(contacts)
 
         return contacts
