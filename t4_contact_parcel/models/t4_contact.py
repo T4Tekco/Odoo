ef02cfdf84e3d022a431243aef50915477af2f2a
TheVy <ntvy2k@gmail.com>
2023-03-21 16:37:14 +0700

    data type stub

--- t4_contact_parcel/models/t4_contact.py
+++ t4_contact_parcel/models/t4_contact.py
@@ -1,3 +1,68 @@
 # -*- coding: utf-8 -*-
 
+from typing import Any
+
 from odoo import api, fields, models
+
+
+class T4Contact(models.Model):
+    _inherit = "res.partner"
+
+    @api.model
+    def parcel(self, data: Any):
+        """
+        data:
+        {
+            "company_name": {
+                "name": string,
+                "foreign": string,
+                "shortname": string
+            },
+            "identity": string,
+            "tax_code: string,
+            "day_of_birth": date,
+            "country_code": string,
+            "headquarters_address": {
+                "street": string,
+                "city": string,
+                "zip": string,
+                "country": string,
+                "phone": string,
+                "fax": string,
+                "email": string,
+                "website": string
+            },
+            "business_sectors": {
+                "main_industry_code": string,
+                "sub_industries": [string, ...]
+            },
+            "charter_capital": float,
+            "registration_office": string,
+            "owners": [people, ...],
+            "legal_representatives": [people, ...]
+        }
+
+        interface people
+        {
+            "name": string,
+            "sex": "male" | "female" | "unknown",
+            "day_of_birth": date,
+            "ethnicity": string,
+            "identity": string,
+            "position": string,
+            "permanent_address": address,
+            "contact_address": address,
+        }
+
+        interface address
+        {
+            "street": string,
+            "street2": string,
+            "city": string,
+            "country_code": string
+        }
+
+        date format: %Y-%m-%d, eg: 2012-12-30
+        """
+
+        return {}
